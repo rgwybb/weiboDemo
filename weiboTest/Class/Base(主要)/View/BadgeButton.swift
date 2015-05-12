3face3dcbf1b5bef8bab0a54e6fa0ef2f175c71f
bbingguo <chinaguobingbing@outlook.com>
2015-05-13 07:11:05 +0800

    one

--- weiboTest/Class/Base(主要)/View/BadgeButton.swift
+++ weiboTest/Class/Base(主要)/View/BadgeButton.swift
@@ -11,34 +11,36 @@ import UIKit
 
 class BadgeButton: UIButton {
 
-    var badgeValue:String{
-        didSet{
-            if(!badgeValue.isEmpty){
-                self.hidden=false
-                self.setTitle(badgeValue, forState: UIControlState.Normal)
-                
-                //设置frame
-                 CGRect thisFrame=self.frame
-                CGFloat badgeH = self.currentBackgroundImage?.size.height
-                CGFloat badgeW = self.currentBackgroundImage?.size.width
-                if (count(badgeValue)>0){
-                    // 文字的尺寸
-                    CGSize badgeSize = [badgeValue sizeWithFont:self.titleLabel.font];
-                    badgeW = badgeSize.width + 10;
-                    frame.size.width = badgeW;
-                    frame.size.height = badgeH;
-                    self.frame = frame;
-
-                }else {
-                    self.hidden=true
-                }
-            }
-            
-        }
-    }
-    override init(frame: CGRect) {
-        //self.titleLabel?.text=""
+//    var badgeValue:String{
+//        didSet{
+//            if(!badgeValue.isEmpty){
+//                self.hidden=false
+//                self.setTitle(badgeValue, forState: UIControlState.Normal)
+//                
+//                //设置frame
+//                 CGRect.self thisFrame=self.frame
+//                CGFloat badgeH = self.currentBackgroundImage?.size.height
+//                CGFloat badgeW = self.currentBackgroundImage?.size.width
+//                if (count(badgeValue)>0){
+//                    // 文字的尺寸
+//                    CGSize badgeSize = [badgeValue sizeWithFont:self.titleLabel.font];
+//                    badgeW = badgeSize.width + 10;
+//                    frame.size.width = badgeW;
+//                    frame.size.height = badgeH;
+//                    self.frame = frame;
+//
+//                }else {
+//                    self.hidden=true
+//                }
+//            }
+//            
+//        }
+//    }
+    required override init(frame: CGRect) {
         super.init(frame: frame)
+        self.hidden=true
+        self.userInteractionEnabled=false
+        self.setBackgroundImage(UIImage.aa())), forState: UIControlState.Normal)
     }
 
     required init(coder aDecoder: NSCoder) {
--- weiboTest/Class/Other(其他)/extension/Extension.swift
+++ weiboTest/Class/Other(其他)/extension/Extension.swift
@@ -12,5 +12,14 @@ import UIKit
 *  image扩展方法
 */
 extension UIImage {
-   
+    func resizedImageWithName(name:String)->UIImage {
+        var image=UIImage(named: name)
+        var leftCapWidth:Int = Int(image!.size.width * 0.5)
+        var topCapHeight:Int = Int(image!.size.height * 0.5)
+       return image!.stretchableImageWithLeftCapWidth(leftCapWidth, topCapHeight: topCapHeight)
+       
+    }
+    func aa(ss:String)->String {
+        return "s"
+    }
 }
